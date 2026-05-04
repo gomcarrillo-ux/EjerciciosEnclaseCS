ee57e2d2b895a44efc03323c3b3837870bb7130b
gabrielito <gomcarrillo@uamv.edu.ni>
2026-05-04 12:16:40 -0600

    Ejercicio while1

---
+++ while1/Program.cs
@@ -0,0 +1,22 @@
+﻿int numero;
+while (true)
+{
+    Console.Write("ingrese un numero entero: ");
+    if (int.TryParse(Console.ReadLine(), out numero))
+    {
+        break;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("entrada no valida. Por favor, ingrese un numero entero.");
+        Console.ResetColor();
+    }
+}
+Console.WriteLine("Mostrando el cuadrado de los antecesores de " + numero + ":");
+int cont = 1;
+while(cont < numero)
+{
+    Console.WriteLine("El cuadrado de " + cont + " es: " + Math.Pow(cont, 2));
+    cont++;
+}
